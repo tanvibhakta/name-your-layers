a6a5d6f61e14797c06ec4082c993f1837e51b88a
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-02-17 17:46:58 +0530

    add code to rename selected layers

--- code.ts
+++ code.ts
@@ -14,18 +14,24 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+  if (msg.type === 'rename-layers') {
+    const nodes = figma.currentPage.selection;
+    nodes.forEach(node => {
+      console.log(node)
+      if (node.type === "FRAME" || node.type === "GROUP" || node.type === "COMPONENT") {
+        node.name = "test";
+      }
+    });
+      }
+    // for (let i = 0; i < msg.count; i++) {
+    //   const rect = figma.createRectangle();
+    //   rect.x = i * 150;
+    //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    //   figma.currentPage.appendChild(rect);
+    //   nodes.push(rect);
+    // }
+    // figma.currentPage.selection = nodes;
+    // figma.viewport.scrollAndZoomIntoView(nodes);
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
